089246a92b1c9a7e9f0aff6f21ccfa66edeb34a5
1dNDN <idndn@inbox.ru>
2021-03-29 14:01:17 +0300

    Добавьте файлы проекта.

---
+++ ddns/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ddns
+{
+	class Program
+	{
+		static void Main(string[] args)
+        {
+            string LastIPFilePath = Environment.GetEnvironmentVariable(nameof(LastIPFilePath));
+            string LogIPFilePath = Environment.GetEnvironmentVariable(nameof(LogIPFilePath));
+            string RawIPSource = Environment.GetEnvironmentVariable(nameof(RawIPSource));
+        }
+	}
+}
